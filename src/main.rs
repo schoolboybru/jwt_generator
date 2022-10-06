661aaa5aae26fe5b23f1bba609c795ebcc95573c
schoolboybru <brandon.bachynski@gmail.com>
2022-10-06 00:11:01 -0600

    Error handling

--- src/main.rs
+++ src/main.rs
@@ -16,27 +16,28 @@ fn get_arguments() {
     let matches = Command::new("JWT Generator")
         .version("0.1.0")
         .author("Brandon Bachynski")
+        .arg_required_else_help(true)
         .arg(Arg::with_name("file")
             .short('f')
             .long("file")
             .takes_value(true)
-            .help("A file"))
+            .help("A file to be read"))
         .get_matches();
 
-    let myfile = matches.value_of("file").unwrap_or("input.txt");
+    let myfile = matches.value_of("file").expect("Invalid file");
 
-    let config = read_file(myfile.to_string());
+    let config = read_file(myfile.to_string()).expect("Unable to read file");
 
-    let jwt = create_jwt(config.unwrap());
+    let jwt = create_jwt(config);
 
     println!("{:?}", jwt.unwrap());
 }
 
 fn create_jwt(config: Outer) -> Result<String>  {
     let payload = &config.payload;
-   let result = encode(&Header::default(), payload, &EncodingKey::from_secret("secret".as_ref())).unwrap(); 
+    let result = encode(&Header::default(), payload, &EncodingKey::from_secret("secret".as_ref())).unwrap(); 
 
-   Ok(result)
+    Ok(result)
 }
 
 fn read_file(file: String) -> Result<Outer> {
